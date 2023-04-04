0485f993fcd2e873c0037ff46980449ecdb56a82
ikdmtm <ikdmtm01@gmail.com>
2023-04-05 05:02:53 +0900

    ✨ ログイン状態をstateで管理

--- open-street-walk/src/pages/_app.jsx
+++ open-street-walk/src/pages/_app.jsx
@@ -1,5 +1,16 @@
+import { useEffect, useState } from "react";
 import "src/styles/globals.css";
+import Cookies from "js-cookie";
+import { useSignOut } from "src/hooks/useSignOut";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const authSignOut = useSignOut();
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    const token = Cookies.get("uid");
+    if (token) {
+      setIsLogin(true);
+    }
+  }, []);
+  return <Component {...pageProps} isLogin={isLogin} setIsLogin={setIsLogin} authSignOut={authSignOut}/>;
 }
