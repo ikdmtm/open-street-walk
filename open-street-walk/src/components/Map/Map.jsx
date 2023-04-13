5ddf039bd159b4116a1acbd02fac9d14dd82b4f8
ikdmtm <ikdmtm01@gmail.com>
2023-04-13 14:59:03 +0900

    ♻️ APIから受け取ったデータを表示

--- open-street-walk/src/components/Map/Map.jsx
+++ open-street-walk/src/components/Map/Map.jsx
@@ -1,10 +1,20 @@
 import "leaflet/dist/leaflet.css";
-import { MapContainer, Marker, Popup, TileLayer, useMapEvent, CircleMarker } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvent,
+  useMapEvents,
+  CircleMarker,
+  useMap,
+} from "react-leaflet";
 import styles from "src/styles/Home.module.css";
 import L from "leaflet";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import { useState } from "react";
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
@@ -14,7 +24,8 @@ L.Icon.Default.mergeOptions({
 });
 
 const Map = (props) => {
-  const position = [35.5623, 139.7151];
+  const position = [35.685, 139.755]; //中心
+  const zoom = 11;
 
   const SetViewOnClick = () => {
     const map = useMapEvent("click", (e) => {
@@ -25,19 +36,45 @@ const Map = (props) => {
     return null;
   };
 
+  //現在地にピンを指して中心に移動
+  // const LocationMarker = () => {
+  //   const [position, setPosition] = useState(null);
+  //   const map = useMapEvents({
+  //     click() {
+  //       map.locate();
+  //     },
+  //     locationfound(e) {
+  //       setPosition(e.latlng);
+  //       map.flyTo(e.latlng, map.getZoom());
+  //     },
+  //   });
+  //
+  //   return position === null ? null : (
+  //     <Marker position={position}>
+  //       <Popup>You are here</Popup>
+  //     </Marker>
+  //   );
+  // };
+
   return (
-    <MapContainer className={styles.map} center={position} zoom={17} scrollWheelZoom={true}>
+    <MapContainer
+      className={styles.map}
+      center={position}
+      zoom={zoom}
+      scrollWheelZoom={true}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <SetViewOnClick />
-      {props.pins.length
-        ? props.pins.map((pin) => {
+      {/* <SetViewOnClick /> */}
+      {props.pinsData.length
+        ? props.pinsData.map((pin) => {
             return (
-              <Marker position={pin} key={pin[0]}>
+              <Marker position={[pin.lat, pin.lng]} key={pin.id}>
                 <Popup>
-                  {pin[0]}, {pin[1]}
+                  <p>{pin.title}</p>
+                  <p>{pin.created_at}</p>
                 </Popup>
               </Marker>
               // <CircleMarker center={pin} pathOptions={{ fillColor: "blue" }} radius={5}>
@@ -46,6 +83,7 @@ const Map = (props) => {
             );
           })
         : null}
+      {/* <LocationMarker /> */}
     </MapContainer>
   );
 };
