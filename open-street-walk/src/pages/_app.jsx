887a150dbdef3e571f51a41fc0d91c4593642875
ikdmtm <ikdmtm01@gmail.com>
2023-04-13 14:49:46 +0900

    ♻️ propsの渡し方を変更

--- open-street-walk/src/pages/_app.jsx
+++ open-street-walk/src/pages/_app.jsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
-import "src/styles/globals.css";
 import Cookies from "js-cookie";
-import { useSignOut } from "src/hooks/useSignOut";
+import { useSignout } from "src/hooks/useSignout";
+import "src/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  const authSignOut = useSignOut();
-  const [isLogin, setIsLogin] = useState(false);
+const App = ({ Component, pageProps }) => {
+  const authSignout = useSignout();
+  // const [isLogin, setIsLogin] = useState(false);
   useEffect(() => {
     const token = Cookies.get("uid");
     if (token) {
-      setIsLogin(true);
+      authSignout.setIsLogin(true);
     }
   }, []);
-  return <Component {...pageProps} isLogin={isLogin} setIsLogin={setIsLogin} authSignOut={authSignOut}/>;
-}
+  return <Component {...pageProps} {...authSignout} />;
+};
+
+export default App;
