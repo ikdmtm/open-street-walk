60edb62e355e187c6139103086f0e493d62ae221
ikdmtm <ikdmtm01@gmail.com>
2023-04-15 03:52:57 +0900

    ♻️ マップの読み込み中メッセージを変更

--- open-street-walk/src/components/Map/index.jsx
+++ open-street-walk/src/components/Map/index.jsx
@@ -2,7 +2,7 @@ import dynamic from "next/dynamic";
 import styles from "src/styles/Home.module.css";
 
 const Map = dynamic(() => import("./Map"), {
-  loading: () => <div className={styles.map}>A map is loading</div>,
+  loading: () => <div className={styles.map}>読み込み中</div>,
   ssr: false,
 });
 
