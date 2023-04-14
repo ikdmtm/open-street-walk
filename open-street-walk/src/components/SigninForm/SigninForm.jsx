20ad8d6936213d52526bf593612c0361dd4f7de5
ikdmtm <ikdmtm01@gmail.com>
2023-04-15 03:57:35 +0900

    ♻️ 処理をコンポーネント側に移動

--- open-street-walk/src/components/SigninForm/SigninForm.jsx
+++ open-street-walk/src/components/SigninForm/SigninForm.jsx
@@ -1,8 +1,63 @@
 import styles from "src/components/SigninForm/SigninForm.module.css";
 import Link from "next/link";
+import { useCallback, useState } from "react";
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
 
 export const SigninForm = (props) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter();
+
+  //input
+  const handleEmail = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+  const handlePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+
+  //ログイン処理
+  const url = `${process.env.NEXT_PUBLIC_API_URL}` + "/auth/sign_in";
+  const handleAuth = async () => {
+    if (password && email) {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: `${email}`, password: password }),
+      };
+      try {
+        const res = await fetch(url, options);
+        if (!res.ok) {
+          throw new Error("ログイン失敗");
+        }
+        console.log("ログイン成功");
+        const data = await res.json();
+        //ログイン状態の変更
+        props.setIsLogin(true);
+        const uidData = res.headers.get("uid");
+        const clientData = res.headers.get("client");
+        const accessTokenData = res.headers.get("access-token");
+        //ログイン成功でクッキーのセット
+        Cookies.set("uid", uidData, { expires: 14 });
+        Cookies.set("client", clientData, { expires: 14 });
+        Cookies.set("access-token", accessTokenData, { expires: 14 });
+        router.push("/"); //redirect
+        console.log(data, uidData, clientData, accessTokenData);
+      } catch (error) {
+        console.error(error);
+        //ログイン失敗でクッキーの削除
+        Cookies.remove("uid");
+        Cookies.remove("client");
+        Cookies.remove("access-token");
+      }
+    } else {
+      setErrorMessage("入力に誤りがあります");
+    }
+  };
   return (
     <div className={styles.inputForm}>
       <h1 className={styles.formTitle}>ログイン</h1>
@@ -11,8 +66,8 @@ export const SigninForm = (props) => {
         <p className={styles.inputTitle}>メールアドレス</p>
         <input
           type="email"
-          value={props.email}
-          onChange={props.handleEmail}
+          value={email}
+          onChange={handleEmail}
           className={styles.input}
         ></input>
       </div>
@@ -20,13 +75,13 @@ export const SigninForm = (props) => {
         <p className={styles.inputTitle}>パスワード</p>
         <input
           type="password"
-          value={props.password}
-          onChange={props.handlePassword}
+          value={password}
+          onChange={handlePassword}
           className={styles.input}
         ></input>
       </div>
       <div className={styles.inputGroupe}>
-        <button className={styles.button} onClick={props.handleAuth}>
+        <button className={styles.button} onClick={handleAuth}>
           ログイン
         </button>
       </div>
--- open-street-walk/src/components/SignupForm/SignupForm.jsx
+++ open-street-walk/src/components/SignupForm/SignupForm.jsx
@@ -1,8 +1,68 @@
 import styles from "src/components/SignupForm/SignupForm.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useCallback, useState } from "react";
+import Cookies from "js-cookie";
 
 export const SignupForm = (props) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmation, setConfirmation] = useState();
   const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter();
+
+  //input
+  const handleEmail = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+  const handlePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+  const handleConfirmation = useCallback((e) => {
+    setConfirmation(e.target.value);
+  }, []);
+
+  console.log(email, password, confirmation);
+  //新規登録処理
+  const url = `${process.env.NEXT_PUBLIC_API_URL}` + "/auth";
+  const handleAuth = async () => {
+    if (email && password && confirmation && password == confirmation) {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: `${email}`, password: password }), //入力された値を入れるように変更
+      };
+      try {
+        const res = await fetch(url, options);
+        if (!res.ok) {
+          throw new Error("新規登録失敗");
+        }
+        console.log("新規登録成功");
+        const data = await res.json();
+        //ログイン状態の変更
+        props.setIsLogin(true);
+        const uidData = res.headers.get("uid");
+        const clientData = res.headers.get("client");
+        const accessTokenData = res.headers.get("access-token");
+        //新規登録成功でクッキーのセット
+        Cookies.set("uid", uidData);
+        Cookies.set("client", clientData);
+        Cookies.set("access-token", accessTokenData);
+        router.push("/"); //redirect
+        console.log(data, uidData, clientData, accessTokenData);
+      } catch (error) {
+        console.error(error);
+        //新規登録失敗でクッキーの削除
+        Cookies.remove("uid");
+        Cookies.remove("client");
+        Cookies.remove("access-token");
+      }
+    } else {
+      setErrorMessage("入力に誤りがあります");
+    }
+  };
   return (
     <div className={styles.inputForm}>
       <h1 className={styles.formTitle}>新規登録</h1>
@@ -11,8 +71,8 @@ export const SignupForm = (props) => {
         <p className={styles.inputTitle}>メールアドレス</p>
         <input
           type="email"
-          value={props.email}
-          onChange={props.handleEmail}
+          value={email}
+          onChange={handleEmail}
           className={styles.input}
         ></input>
       </div>
@@ -20,8 +80,8 @@ export const SignupForm = (props) => {
         <p className={styles.inputTitle}>パスワード</p>
         <input
           type="password"
-          value={props.password}
-          onChange={props.handlePassword}
+          value={password}
+          onChange={handlePassword}
           className={styles.input}
         ></input>
       </div>
@@ -29,13 +89,13 @@ export const SignupForm = (props) => {
         <p className={styles.inputTitle}>パスワード確認</p>
         <input
           type="password"
-          value={props.confirmation}
-          onChange={props.handleConfirmation}
+          value={confirmation}
+          onChange={handleConfirmation}
           className={styles.input}
         ></input>
       </div>
       <div className={styles.inputGroupe}>
-        <button className={styles.button} onClick={props.handleAuth}>
+        <button className={styles.button} onClick={handleAuth}>
           新規登録
         </button>
       </div>
