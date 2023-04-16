5b72ce79f122402287894ececfdfa5cfb553b88b
ikdmtm <ikdmtm01@gmail.com>
2023-04-16 11:42:48 +0900

    ♻️ 画像のURLとlatlngの空を修正

--- open-street-walk/src/components/Map/Map.jsx
+++ open-street-walk/src/components/Map/Map.jsx
@@ -16,6 +16,7 @@ import L from "leaflet";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import { useState, useRef } from "react";
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
@@ -25,7 +26,7 @@ L.Icon.Default.mergeOptions({
 });
 
 const Map = (props) => {
-  const position = [35.685, 139.755]; //中心
+  const [position, setPosition] = useState([35.685, 139.755]); //中心
   const zoom = 11;
 
   const SetViewOnClick = () => {
@@ -37,35 +38,81 @@ const Map = (props) => {
     return null;
   };
 
+  const CurrentPosition = () => {
+    const map = useMap();
+    //現在地を取得
+    const getPosition = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            map.flyTo(
+              [position.coords.latitude, position.coords.longitude],
+              17
+            );
+          },
+          () => {
+            alert("現在地の取得に失敗しました");
+          }
+        );
+      } else {
+        console.error("Geolocation is not supported in this browser.");
+      }
+    };
+
+    return (
+      <div className="leaflet-top leaflet-right">
+        <div className="leaflet-control leaflet-bar">
+          <button onClick={getPosition}>現在地周辺に移動</button>
+        </div>
+      </div>
+    );
+  };
+
   const formatFunction = (time) => {
     const createdAt = new Date(time);
     const formattedCreatedAt = format(createdAt, "yyyy-MM-dd  HH:mm");
     return formattedCreatedAt;
   };
 
+  //クリックした場所にピンを作成
+  // const HandleClickMap = () => {
+  //   const map = useMapEvents({
+  //     click(e) {
+  //       console.log(e.latlng);
+  //       setPosition(e.latlng);
+  //       // map.flyTo(e.latlng, 17);
+  //     },
+  //   });
+  //   return position && position.lat && position.lng ? (
+  //     <Marker position={position}>
+  //       <Popup>
+  //         <p>
+  //           {position.lat.toFixed(4)}, {position.lng.toFixed(4)}
+  //         </p>
+  //       </Popup>
+  //     </Marker>
+  //   ) : null;
+  // };
+
   //現在地にピンを指して中心に移動
   // const LocationMarker = () => {
-  //   const [position, setPosition] = useState(null);
+  //   const [clickPosition, setClickPosition] = useState(null);
   //   const map = useMapEvents({
   //     click() {
   //       map.locate();
   //     },
   //     locationfound(e) {
-  //       setPosition(e.latlng);
-  //       map.flyTo(e.latlng, map.getZoom());
+  //       setClickPosition(e.latlng);
+  //       map.flyTo(e.latlng, 17);
   //     },
   //   });
-  //
+
   //   return position === null ? null : (
-  //     <Marker position={position}>
+  //     <Marker position={ClickPosition}>
   //       <Popup>You are here</Popup>
   //     </Marker>
   //   );
   // };
-
-  const clickMarker = () => {
-    return <p>test</p>;
-  };
 
   return (
     <MapContainer
@@ -78,8 +125,7 @@ const Map = (props) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {/* <SetViewOnClick /> */}
-      {props.pinsData.pins.length
+      {props.pinsData && props.pinsData.pins && props.pinsData.pins.length
         ? props.pinsData.pins.map((pin) => {
             return (
               <Marker position={[pin.lat, pin.lng]} key={pin.id}>
@@ -88,7 +134,10 @@ const Map = (props) => {
                   {props.pinsData.pin_urls[pin.id - 1] ? (
                     <div className={styles.image}>
                       <Image
-                        src={props.pinsData.pin_urls[pin.id - 1]}
+                        src={
+                          process.env.NEXT_PUBLIC_API_URL +
+                          props.pinsData.pin_urls[pin.id - 1]
+                        }
                         alt="画像"
                         layout="fill"
                         objectFit="contain"
@@ -107,7 +156,10 @@ const Map = (props) => {
             );
           })
         : null}
+      {/* <SetViewOnClick /> */}
       {/* <LocationMarker /> */}
+      {/* <HandleClickMap /> */}
+      <CurrentPosition />
     </MapContainer>
   );
 };
